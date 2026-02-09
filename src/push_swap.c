64f5f19311ab0e0df275faf7741ef579664fc422
khazink <khairellkhazin@gmail.com>
2026-02-09 18:38:41 +0800

    push_swap:sorted and index parse

--- src/push_swap.c
+++ src/push_swap.c
@@ -1,6 +1,24 @@
 #include "libft.h"
 #include "push_swap.h"
 
+int	is_sorted(t_stack *stack)
+{
+	if (!stack)
+		return (1);
+	while (stack)
+	{
+		if (stack->value > stack->next->value)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
+}
+
+void	assign_index(t_stack *stack)
+{
+	
+}
+
 int	main(int argc, char **argv)
 {
 	t_stack	*a;
@@ -11,6 +29,12 @@ int	main(int argc, char **argv)
 	if (argc < 2)
 		return (0);
 	a = parse_arg(argc, argv);
+	if (is_sorted(a))
+	{
+		free_stack(&a);
+		return (0);
+	}
+	assign_index(a);
 	size = ft_lstsize(a);
 	if (size <= 5)
 		sort_small(a, b);
